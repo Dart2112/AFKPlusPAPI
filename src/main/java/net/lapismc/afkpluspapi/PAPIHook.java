2302644668fa37cb86ee6d433692ab5dc4ee41c9
Benjamin Martin <DNAblue2112@gmail.com>
2020-12-08 17:13:05 +1030

    Updated to latest AFKPlus Updated to 1.16.4 Added placeholder for TotalAFKTime

--- src/main/java/net/lapismc/afkpluspapi/PAPIHook.java
+++ src/main/java/net/lapismc/afkpluspapi/PAPIHook.java
@@ -4,10 +4,14 @@ import net.lapismc.afkplus.AFKPlus;
 import net.lapismc.afkplus.api.AFKPlusPlayerAPI;
 import net.lapismc.afkplus.playerdata.AFKPlusPlayer;
 import net.lapismc.afkplus.util.core.placeholder.PlaceholderAPIExpansion;
+import net.lapismc.afkplus.util.prettytime.Duration;
+import net.lapismc.afkplus.util.prettytime.PrettyTime;
 import org.bukkit.entity.Player;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.util.Date;
+import java.util.List;
 
 public class PAPIHook extends PlaceholderAPIExpansion {
 
@@ -35,7 +39,30 @@ public class PAPIHook extends PlaceholderAPIExpansion {
             Double minutes = millis / 1000.0 / 60.0;
             NumberFormat numberFormat = new DecimalFormat("#.##");
             return numberFormat.format(minutes) + " " + plugin.getConfig().getString("afktime.minutes");
+        } else if ("TotalTimeAFK".equalsIgnoreCase(identifier)) {
+            AFKPlusPlayer p = api.getPlayer(player);
+            //Get the total time in milliseconds
+            long totalTime = p.getTotalTimeAFK();
+            //Get the list of durations for this time difference, reduce that list to the configured amount
+            List<Duration> totalTimeDurations = reduceDurationList(new PrettyTime(
+                    new Date(0)).calculatePreciseDuration(new Date(totalTime)));
+            //Get pretty time to format the remaining durations without future or past context
+            return new PrettyTime().formatDuration(totalTimeDurations);
         }
         return null;
     }
+
+    private List<Duration> reduceDurationList(List<Duration> durationList) {
+        while (durationList.size() > plugin.getConfig().getInt("TotalTimeAFK.numberOfTimeUnits")) {
+            Duration smallest = null;
+            for (Duration current : durationList) {
+                if (smallest == null || smallest.getUnit().getMillisPerUnit() > current.getUnit().getMillisPerUnit()) {
+                    smallest = current;
+                }
+            }
+            durationList.remove(smallest);
+        }
+        return durationList;
+    }
+
 }
