df31b8cf6b2cfe9ff5390324fae51bd0497425a5
Benjamin <DNAblue2112@gmail.com>
2024-09-06 14:25:55 +0930

    Update 1.0.6 Fixed errors in PrettyTime

--- src/main/java/net/lapismc/afkpluspapi/PAPIHook.java
+++ src/main/java/net/lapismc/afkpluspapi/PAPIHook.java
@@ -4,11 +4,13 @@ import net.lapismc.afkplus.AFKPlus;
 import net.lapismc.afkplus.api.AFKPlusPlayerAPI;
 import net.lapismc.afkplus.playerdata.AFKPlusPlayer;
 import net.lapismc.afkplus.util.core.placeholder.PlaceholderAPIExpansion;
+import net.lapismc.afkplus.util.core.utils.prettytime.Duration;
+import net.lapismc.afkplus.util.core.utils.prettytime.PrettyTime;
+import net.lapismc.afkplus.util.core.utils.prettytime.units.JustNow;
+import net.lapismc.afkplus.util.core.utils.prettytime.units.Millisecond;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
-import org.ocpsoft.prettytime.Duration;
-import org.ocpsoft.prettytime.PrettyTime;
 
 import java.util.Date;
 import java.util.List;
@@ -17,11 +19,15 @@ public class PAPIHook extends PlaceholderAPIExpansion {
 
     private final AFKPlusPlayerAPI api;
     private final AFKPlusPAPI plugin;
+    private final PrettyTime prettyTime;
 
     public PAPIHook(AFKPlusPAPI plugin) {
         super(AFKPlus.getInstance());
         this.plugin = plugin;
         api = new AFKPlusPlayerAPI();
+        prettyTime = new PrettyTime();
+        prettyTime.removeUnit(JustNow.class);
+        prettyTime.removeUnit(Millisecond.class);
     }
 
     @Override
@@ -36,10 +42,9 @@ public class PAPIHook extends PlaceholderAPIExpansion {
                 return plugin.getConfig().getString("afktime.notafk");
             }
             //Get the list of durations for this time difference, reduce that list to the configured amount
-            List<Duration> totalTimeDurations = reduceDurationList(new PrettyTime(
-                    new Date(0)).calculatePreciseDuration(new Date(afkStart)));
+            List<Duration> totalTimeDurations = reduceDurationList(prettyTime.calculatePreciseDuration(new Date(afkStart)));
             //Get pretty time to format the remaining durations without future or past context
-            return new PrettyTime().formatDuration(totalTimeDurations);
+            return prettyTime.formatDuration(totalTimeDurations);
         } else if ("TotalTimeAFK".equalsIgnoreCase(identifier)) {
             AFKPlusPlayer p = api.getPlayer(player);
             //Get the total time in milliseconds
@@ -48,7 +53,7 @@ public class PAPIHook extends PlaceholderAPIExpansion {
             List<Duration> totalTimeDurations = reduceDurationList(new PrettyTime(
                     new Date(0)).calculatePreciseDuration(new Date(totalTime)));
             //Get pretty time to format the remaining durations without future or past context
-            return new PrettyTime().formatDuration(totalTimeDurations);
+            return prettyTime.formatDuration(totalTimeDurations);
         } else if ("PlayersAFK".equalsIgnoreCase(identifier)) {
             int AFKPlayerCount = 0;
             for (Player p : Bukkit.getOnlinePlayers()) {
